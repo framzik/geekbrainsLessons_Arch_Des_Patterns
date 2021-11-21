70e15525e4a824b40e17c2db221b2d6bb2edd4c5
framzik <framzik@gmail.com>
2021-11-21 17:54:55 +0300

    hw3

---
+++ hw3/src/main/java/ru/khrebtov/OrderLineItemDto.java
@@ -0,0 +1,93 @@
+package ru.khrebtov;
+
+/**
+ * 1. Реализовать как минимум один порождающий паттерн в своем домашнем приложении.
+ */
+public class OrderLineItemDto {
+
+    private Long productId;
+
+    private Long orderId;
+
+    private Integer qty;
+
+    private String color;
+
+    private String material;
+
+    private OrderLineItemDto(Builder builder) {
+        this.productId = builder.productId;
+        this.orderId = builder.orderId;
+        this.qty = builder.qty;
+        this.color = builder.color;
+        this.material = builder.material;
+    }
+
+    public Long getProductId() {
+        return productId;
+    }
+
+    public Long getOrderId() {
+        return orderId;
+    }
+
+    public Integer getQty() {
+        return qty;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    @Override
+    public String toString() {
+        return "OrderLineItemDto{" +
+                "productId=" + productId +
+                ", orderId=" + orderId +
+                ", qty=" + qty +
+                ", color='" + color + '\'' +
+                ", material='" + material + '\'' +
+                '}';
+    }
+
+    public static class Builder {
+
+        private Long productId;
+
+        private Long orderId;
+
+        private Integer qty;
+
+        private String color;
+
+        private String material;
+
+        public Builder(Long productId, Long orderId) {
+            this.productId = productId;
+            this.orderId = orderId;
+        }
+
+        public Builder qty(Integer qty) {
+            this.qty = qty;
+            return this;
+        }
+
+        public Builder color(String color) {
+            this.color = color;
+            return this;
+        }
+
+        public Builder material(String material) {
+            this.material = material;
+            return this;
+        }
+
+        public OrderLineItemDto build() {
+            return new OrderLineItemDto(this);
+        }
+    }
+}
